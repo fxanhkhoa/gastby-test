2418532ef6011fd7c3d7c82093417f12f02e9a36
Khoa <khoa@wearebloom.com.au>
2022-08-22 11:33:39 +0700

    test axios

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-source-rest-api",
+      options: {
+        endpoints: [
+          "https://api.nytimes.com/",
+          "https://jsonplaceholder.typicode.com/posts",
+        ],
+      },
+    },
   ],
 }
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,14 +3,29 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import axios from "axios"
 
-const SecondPage = () => (
-  <Layout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const SecondPage = () => {
+  const [data, setData] = React.useState(undefined)
+  React.useEffect(() => {
+    axios
+      .get(
+        "https://api.publicapis.org/entries"
+      )
+      .then(res => {
+        console.log(res.data)
+        setData(res.data)
+      })
+  }, [])
+  return (
+    <Layout>
+      <h1>{data.count}</h1>
+      <h1>Hi from the second page</h1>
+      <p>Welcome to page 2</p>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
+}
 
 export const Head = () => <Seo title="Page two" />
 
